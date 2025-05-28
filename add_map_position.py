daaf29cb61d0ae5b8f9c84641daaf2af365c2ef2
Randy <randybrown9812@gmail.com>
2025-05-28 13:17:40 -0500

    Initial Commit

--- add_map_position.py
+++ add_map_position.py
@@ -1,6 +1,24 @@
 import json
+import os
+from typing import List, TypedDict
+
+from dotenv import load_dotenv
+
 import requests
-import xml.etree.ElementTree as ET
+
+class Address(TypedDict):
+    buildingName: str
+    street: str
+    city: str
+    state: str
+    zipCode: str
+
+class StateCapital(TypedDict):
+    state: str
+    capital: str
+    address: Address
+
+
 def app_map_position():
     """
     This function performs these steps:
@@ -10,43 +28,86 @@ def app_map_position():
     4.) Output the JSON of the address object.
     5.) Verify that the JSON file outputted is Valid JSON.
     """
-    verify_json_file("us_capital_addresses.json")
+    state_capitals: List[StateCapital] = grab_state_capital_addresses_from_json("us_capital_addresses.json")
+    verify_state_capital_addresses_from_json(state_capitals)
 
-def verify_json_file(filename: str) -> bool:
+def grab_state_capital_addresses_from_json(filename: str) -> List[StateCapital]:
+    with open(filename, "r") as file:
+        json_contents = json.load(file)
+
+    state_capital_addresses: List[StateCapital] = json_contents["stateCapitols"]
+    return state_capital_addresses
+
+def verify_state_capital_addresses_from_json(state_capitals: List[StateCapital]) -> bool:
     """
     Verify the JSON file using the USPS API.
     USPS API only allows XML.
     """
-    with open(filename, "r") as file:
-        addresses = json.load(file)
 
     api_batch_size = 5
-    for index in range(0, len(addresses), api_batch_size):
-        batch = addresses[index: index + api_batch_size]
+    for index in range(0, len(state_capitals), api_batch_size):
+        batch = state_capitals[index: index + api_batch_size]
         xml_addresses = ""
-        for batch_index, full_address in enumerate(batch):
-            address, city, state = full_address.split(", ")
-            state: str = state[:2]
+        for batch_index, state_capital in enumerate(batch):
             xml_addresses += f"""
-            <Address ID="{batch_index}">
-              <Address1>{address}</Address1>
-              <Address2></Address2>
-              <State>{state}</State>
-              <Zip5></Zip5>
-              <Zip4></Zip4>
-            </Address>
+                <Address ID="{batch_index}">
+                  <Address1>{state_capital.get("address", {}).get("street")}</Address1>
+                  <Address2></Address2>
+                  <City>{state_capital.get("address", {}).get("city")}</City>
+                  <State>{state_capital.get("state")}</State>
+                  <Zip5></Zip5>
+                  <Zip4></Zip4>
+                </Address>
+                """
+
+        xml_batch_request = f"""
+            <AddressValidateRequest USERID="{grab_user_id_env()}">
+              {xml_addresses}
+            </AddressValidateRequest>
             """
 
-        test = "a"
-        xml_batch_request = f"""
-        <AddressValidateRequest USERID="{test}">
-        {xml_addresses}
-        </AddressValidateRequest>
-        """
+        url = f"https://secure.shippingapis.com/ShippingAPI.dll?API=Verify&XML={encoded_xml}"
 
-        print(xml_batch_request)
+        response = requests.get(url)
+        response.raise_for_status()
 
+        root = ET.fromstring(response.content)
 
+        for batch_index, address in enumerate(root.findall('address')):
+            error = address.find('Error')
+            if error is not None:
+                desc = error.findtext('Description')
+                print(f"Address contains Error! Address:{batch[batch_index]} Error: {desc}")
+                return False
+
+            response_address2 = address.findtext('secondaryAddress', default='')
+            response_city = address.findtext('city', default='')
+            response_state = address.findtext('state', default='')
+            response_zip_code = address.findtext('ZIPCode', default='')
+            batch_address = batch[batch_index].get('address', {}).get('street')
+            batch_city = batch[batch_index].get('address', {}).get('city')
+            batch_state = batch[batch_index].get('state')
+            batch_zip_code = batch[batch_index].get('address', {}).get('zipCode')
+
+            if batch_state != response_state:
+                print(f"JSON State Capital State: {batch_state} does not match {response_state} from USPS API ")
+                return False
+            elif batch_city != response_city:
+                print(f"JSON State Capitals City: {batch_city} does not match {response_city} from USPS API ")
+                return False
+            elif batch_address != response_address2:
+                print(f"JSON State Capitals City: {response_address2} does not match {batch_address} from USPS API ")
+                return False
+            elif batch_zip_code != response_zip_code:
+                print(f"JSON State Capitals City: {batch_zip_code} does not match {response_zip_code} from USPS API ")
+                return False
+
+    print(f"JSON Validated Successfully!")
+    return True
+
+def grab_user_id_env() -> str | None:
+    return os.getenv("USERID")
 
 if __name__ == "__main__":
+    load_dotenv()
     app_map_position()
